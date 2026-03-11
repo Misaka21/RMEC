df88dd851ee75b31c149e1d1a62d68808533f048
Misaka21 <31301341+Misaka21@users.noreply.github.com>
2026-03-11 14:42:08 +0800

    🐛 fix(motor_task): Subscribe 提前至 pre-scheduler, 增加板级条件编译
    1. Init() 从 init_func 移至 MotorTaskStart() 函数体, 满足 Topic 时序约束
    2. 按 ONE_BOARD/CHASSIS_BOARD/GIMBAL_BOARD 裁剪电机初始化和 Tick, 对齐 ins_task 模式

--- Core/app/motor_task/motor_task.cpp
+++ Core/app/motor_task/motor_task.cpp
@@ -1,23 +1,39 @@
 #include "motor_task.hpp"
-#include "chassis_motors.hpp"
-#include "gimbal_motors.hpp"
-#include "shoot_motors.hpp"
+#include "robot_def.hpp"
 #include "TaskManager.hpp"
 #include "sal_dwt.h"
 
-// 新增 driver 头文件 (FlushAll 调用)
+// 按板型条件编译子系统
+#if defined(ONE_BOARD) || defined(CHASSIS_BOARD)
+#include "chassis_motors.hpp"
+static ChassisMotors chassis;
+#endif
+
+#if defined(ONE_BOARD) || defined(GIMBAL_BOARD)
+#include "gimbal_motors.hpp"
+#include "shoot_motors.hpp"
+static GimbalMotors gimbal;
+static ShootMotors  shoot;
+#endif
+
+// driver 头文件 (FlushAll 调用)
 #include "dji_driver.hpp"
 #include "dm_driver.hpp"
 #include "ht_driver.hpp"
 #include "lk_driver.hpp"
 
-static ChassisMotors chassis;
-static GimbalMotors  gimbal;
-static ShootMotors   shoot;
-
 static DwtInstance motor_dwt;
 
 void MotorTaskStart() {
+    // Pre-scheduler: Subscribe + 创建电机 (满足 Topic 时序约束)
+#if defined(ONE_BOARD) || defined(CHASSIS_BOARD)
+    chassis.Init();
+#endif
+#if defined(ONE_BOARD) || defined(GIMBAL_BOARD)
+    gimbal.Init();
+    shoot.Init();
+#endif
+
     static TaskManager motor_task({
         .name       = "motor",
         .stack_size = 512,
@@ -25,9 +41,6 @@ void MotorTaskStart() {
         .period_ms  = 1,
 
         .init_func = []() {
-            chassis.Init();
-            gimbal.Init();
-            shoot.Init();
             motor_dwt.DwtGetDeltaT();
         },
 
@@ -35,9 +48,15 @@ void MotorTaskStart() {
             float dt = motor_dwt.DwtGetDeltaT();
 
             // 全部 1kHz 更新
+#if defined(ONE_BOARD) || defined(GIMBAL_BOARD)
             gimbal.Tick(dt);
+#endif
+#if defined(ONE_BOARD) || defined(CHASSIS_BOARD)
             chassis.Tick(dt);
+#endif
+#if defined(ONE_BOARD) || defined(GIMBAL_BOARD)
             shoot.Tick(dt);
+#endif
 
             // 统一 flush 所有驱动类型 (无实例时立即返回)
             DjiDriver::FlushAll();
